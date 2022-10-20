39e5126bb72345d1c468a547ade7abcba0fc03f4
fjasic <filipjasic8@gmail.com>
2022-10-20 13:00:48 +0200

    logging and secrets in js

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -9688,15 +9688,24 @@ const core = __nccwpck_require__(8021)
 const github = __nccwpck_require__(4366)
 
 try {
+    core.debug('debug message')
+    core.warning('warning message')
+    core.error('error message')
+
     const name = core.getInput('who-to-greet')
+    core.setSecret(name)
     console.log('Hello ${name}')
 
     const time = new Date()
     core.setOutput('time', time.toTimeString())
 
+    core.startGroup('logging github object')
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup()
+
+    core.exportVariable('HELLO', "hello")
 } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
 }
 
 })();
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,13 +2,22 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
+    core.debug('debug message')
+    core.warning('warning message')
+    core.error('error message')
+
     const name = core.getInput('who-to-greet')
+    core.setSecret(name)
     console.log('Hello ${name}')
 
     const time = new Date()
     core.setOutput('time', time.toTimeString())
 
+    core.startGroup('logging github object')
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup()
+
+    core.exportVariable('HELLO', "hello")
 } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
 }
