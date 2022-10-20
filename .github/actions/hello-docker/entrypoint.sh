a2b9b8d07a4d937fbeb39155ef2364305cd55137
fjasic <filipjasic8@gmail.com>
2022-10-20 13:49:26 +0200

    custom docker action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+echo "::debug ::Debug message"
+echo "::warning ::warning message"
+echo "::error ::error message"
+
+echo "::add-mask::$1"
+echo "Hello $1"
+
+echo "::set-output name=time::$(date)"
+
+echo "::group::some logs"
+echo "bla"
+echo "::endgroup::"
+
+echo "::set-env name=HELLO::hello"
